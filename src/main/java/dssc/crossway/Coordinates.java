b15b6a4f21ba840ab2d9768b8e2b513d3371f5c8
salvatore <militesalvatore@gmail.com>
2019-12-17 10:26:41 +0100

    commented Coordinates

--- src/main/java/dssc/crossway/Coordinates.java
+++ src/main/java/dssc/crossway/Coordinates.java
@@ -1,9 +1,17 @@
 package dssc.crossway;
 
+/**
+ *  Data structure used to store information about the position of an entity
+ *  in the board
+ *
+ */
+
 public class Coordinates {
+
 
     int x_cord;
     int y_cord;
+
 
     public int getX() {
         return x_cord;
@@ -26,21 +34,35 @@ public class Coordinates {
         this.y_cord = y_cord;
     }
 
+    /**
+     * Considering a 3x3 square centered into the current coordinate
+     * @return the coordinates of the upper-left corner
+     */
     public Coordinates getNorthWest()
     {
         return new Coordinates(x_cord-1, y_cord-1);
     }
 
+    /**
+     * Considering a 3x3 square centered into the current coordinate
+     * @return the coordinates of the upper-right corner
+     */
     public Coordinates getNorthEast()
     {
         return new Coordinates(x_cord+1, y_cord-1);
     }
-
+    /**
+     * Considering a 3x3 square centered into the current coordinate
+     * @return the coordinates of the lower-right corner
+     */
     public Coordinates getSouthEast()
     {
         return new Coordinates(x_cord+1, y_cord+1);
     }
-
+    /**
+     * Considering a 3x3 square centered into the current coordinate
+     * @return the coordinates of the lower-left corner
+     */
     public Coordinates getSouthWest()
     {
         return new Coordinates(x_cord-1, y_cord+1);

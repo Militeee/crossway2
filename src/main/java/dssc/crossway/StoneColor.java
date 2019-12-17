6787724f68105e3d1cd939eab470d78673fdce1f
Davide Scassola <projanko@gmail.com>
2019-12-17 12:36:13 +0100

    commented Validator and Input Handler class, implemented first turn rule.

---
+++ src/main/java/dssc/crossway/StoneColor.java
@@ -0,0 +1,21 @@
+package dssc.crossway;
+
+
+/**
+ * enum of possible color status of a cell.
+ */
+public enum StoneColor {
+    EMPTY,
+    BLACK,
+    WHITE;
+
+    /**
+     * Gives the opposite of a Color.
+     * @return  white if black, return black if white, otherwise returns empty.
+     */
+    public StoneColor getOpposite() {
+        if (this== StoneColor.BLACK) {return StoneColor.WHITE;}
+        else if (this == StoneColor.WHITE) {return StoneColor.BLACK;}
+        return StoneColor.EMPTY;
+    }
+}

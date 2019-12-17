c410694dfe983b487d045dc216e4ddf3b19efce1
Andrea <andrelorenzon@gmail.com>
2019-12-17 11:01:32 +0100

    commented Main class

--- src/main/java/dssc/crossway/Main.java
+++ src/main/java/dssc/crossway/Main.java
@@ -2,6 +2,10 @@ package dssc.crossway;
 
 public class Main {
 
+    /**
+     * Main program. It just starts a new Crossway Game.
+     * @param args ignored.
+     */
     public static void main(String[] args) {
         CrosswayGame g = new CrosswayGame();
         g.start();

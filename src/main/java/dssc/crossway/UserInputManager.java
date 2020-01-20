9e243f52bb6921d097967838c66712f9e8fa6178
Andrea <andrelorenzon@gmail.com>
2020-01-20 16:01:32 +0100

    proposed a draft of requestValidInput() in UserInputManager as comment

--- src/main/java/dssc/crossway/UserInputManager.java
+++ src/main/java/dssc/crossway/UserInputManager.java
@@ -19,21 +19,39 @@ public class UserInputManager {
     }
 
     private int requestValidInput(GoBoard crosswayBoard, char coordinate){
-        int dimension;
-        Boolean dimIsValid;
-        while(true){
+         /*
+         int dimension;
+         do {
+             if (coordinate == 'x') {
+                 consoleMessageWriter.requestXMoveMessage();
+             }
+             else if (coordinate == 'y') {
+                 consoleMessageWriter.requestYMoveMessage();
+             }
+             dimension = consoleInputHandler.askUserForInteger();
+         } while (!(crosswayBoard.isInside( dimension )));
+        */
+
+        int dimension = -1;
+
+        Boolean dimIsValid = true;
+        while(dimIsValid){
+
             if (coordinate == 'x') {
                 consoleMessageWriter.requestXMoveMessage();
             }
             else if (coordinate == 'y') {
                 consoleMessageWriter.requestYMoveMessage();
             }
+
             dimension = consoleInputHandler.askUserForInteger();
             dimIsValid = crosswayBoard.isInside(dimension);
             if (dimIsValid) { break;} // <---------------------this works but can be made much better than this. refactor later.
             else {consoleMessageWriter.writeIllegalValueMessage();}
 
             }
+
+
         return dimension;
     }
 
